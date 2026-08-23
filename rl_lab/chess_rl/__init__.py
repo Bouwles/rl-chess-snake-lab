0c5144e5f6356dc3aa49e3705a3873206bea3cc7
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:29:34 +0400

    chore: export chess move helper

--- rl_lab/chess_rl/__init__.py
+++ rl_lab/chess_rl/__init__.py
@@ -1,5 +1,6 @@
 from .env import ChessSelfPlayEnv
 from .features import board_to_tensor, material_score
+from .moves import describe_move
 from .rendering import piece_family
 
-__all__ = ["ChessSelfPlayEnv", "board_to_tensor", "material_score", "piece_family"]
+__all__ = ["ChessSelfPlayEnv", "board_to_tensor", "describe_move", "material_score", "piece_family"]
