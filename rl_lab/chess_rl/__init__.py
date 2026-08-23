23ed53fa343e7cf317b6d2655d69ce3233e66872
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:21:09 +0400

    chore: export chess rendering helper

--- rl_lab/chess_rl/__init__.py
+++ rl_lab/chess_rl/__init__.py
@@ -1,4 +1,5 @@
 from .env import ChessSelfPlayEnv
 from .features import board_to_tensor, material_score
+from .rendering import piece_family
 
-__all__ = ["ChessSelfPlayEnv", "board_to_tensor", "material_score"]
+__all__ = ["ChessSelfPlayEnv", "board_to_tensor", "material_score", "piece_family"]
