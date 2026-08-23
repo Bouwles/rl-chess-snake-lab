6d3ca579bb6b00fe9a0d7733bd836d8df2858b80
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:33:11 +0400

    chore: export config loader

--- rl_lab/common/__init__.py
+++ rl_lab/common/__init__.py
@@ -1,7 +1,15 @@
 from .checkpoints import checkpoint_path
+from .config import load_json_config
 from .metrics import moving_average
 from .replay import ReplayMemory
 from .schedules import decay_epsilon
 from .seed import seed_everything
 
-__all__ = ["ReplayMemory", "checkpoint_path", "decay_epsilon", "moving_average", "seed_everything"]
+__all__ = [
+    "ReplayMemory",
+    "checkpoint_path",
+    "decay_epsilon",
+    "load_json_config",
+    "moving_average",
+    "seed_everything",
+]
