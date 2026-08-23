f224e3b18ac73191bd47812c5a8519f81b44eca3
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:20:52 +0400

    chore: export Snake helpers

--- rl_lab/snake/__init__.py
+++ rl_lab/snake/__init__.py
@@ -1,4 +1,13 @@
+from .baselines import HeuristicSnakePolicy, RandomSnakePolicy
 from .env import Direction, SnakeEnv
+from .evaluate import evaluate_policy
 from .gym_env import SnakeGymEnv
 
-__all__ = ["Direction", "SnakeEnv", "SnakeGymEnv"]
+__all__ = [
+    "Direction",
+    "HeuristicSnakePolicy",
+    "RandomSnakePolicy",
+    "SnakeEnv",
+    "SnakeGymEnv",
+    "evaluate_policy",
+]
