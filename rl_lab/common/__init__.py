a2821804210574e8ba4c346940518a1dd7f14bd6
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:27:56 +0400

    chore: export schedule helper

--- rl_lab/common/__init__.py
+++ rl_lab/common/__init__.py
@@ -1,6 +1,7 @@
 from .checkpoints import checkpoint_path
 from .metrics import moving_average
 from .replay import ReplayMemory
+from .schedules import decay_epsilon
 from .seed import seed_everything
 
-__all__ = ["ReplayMemory", "checkpoint_path", "moving_average", "seed_everything"]
+__all__ = ["ReplayMemory", "checkpoint_path", "decay_epsilon", "moving_average", "seed_everything"]
