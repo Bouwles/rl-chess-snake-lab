9684f91536f8408fef97c0069f4069b94dd360b1
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-23 14:31:18 +0400

    chore: export chess capture helper

--- rl_lab/chess_rl/__init__.py
+++ rl_lab/chess_rl/__init__.py
@@ -1,6 +1,13 @@
 from .env import ChessSelfPlayEnv
 from .features import board_to_tensor, material_score
-from .moves import describe_move
+from .moves import count_captures, describe_move
 from .rendering import piece_family
 
-__all__ = ["ChessSelfPlayEnv", "board_to_tensor", "describe_move", "material_score", "piece_family"]
+__all__ = [
+    "ChessSelfPlayEnv",
+    "board_to_tensor",
+    "count_captures",
+    "describe_move",
+    "material_score",
+    "piece_family",
+]
